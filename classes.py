Fixed expand method in jacobian

classes.py
@@ -463,14 +463,12 @@ class Jacobian:
                     self.matrix[i + self.n, j + self.n] = abs(buses[i + 1].voltage) * (self.y_bus[i, j].real * np.sin(buses[i + 1].delta - buses[j + 1].delta) - self.y_bus[i, j].imag * np.cos(buses[i + 1].delta - buses[j + 1].delta))
             #j_offset = 1
 
-    def continium_expand(self, parameter, alpha_list, beta_list):
+    def continium_expand(self, parameter, beta_list, alpha_list):
         """
         This method is used for Continium Power Flow where the jacobian matrix is expanded with one row and one column
 
         Parameter should be a string type input with value either "voltage" or "load". "load" input is also used in
         predictor phase.
-
-        Note that these values should be removable aswell
         """
         self.cols = self.m + 1
         self.rows = self.m + 1
@@ -478,18 +476,16 @@ class Jacobian:
         new_col = [0] * self.m
         for i in range(len(beta_list)):
             new_col[i] = [beta_list[i]]
-            print("i: ",i)
         for j in range(1, len(alpha_list) +1):
-            print("i+j = ", i+j)
             new_col[i+j] = [alpha_list[j-1]] # offset with i
         # Add row element which is based on phase
         if parameter == "load":
-            new_row[-1] = [1] # index -1 is the last element ie. the diagonal element
+            new_row[-1] = 1 # index -1 is the last element ie. the diagonal element
         elif parameter == "voltage":
-            new_row[-2] = [1] # index -2 is the second last element ie. the last voltage value (in 3 bus system bus 2)
+            new_row[-2] = 1 # index -2 is the second last element ie. the last voltage value (in 3 bus system bus 2)
         else: print("Error: The parameter in Jacobian.continium_expand is not a valid input.")
-        self.matrix = np.append(self.matrix , new_col, 1) # new_col is in format [[],[],[]]
-        self.matrix = np.append(self.matrix, [new_row], 0)  # Add the new row at the bottom
+        self.matrix = np.hstack([self.matrix, new_col]) # Add the new column
+        self.matrix = np.vstack([self.matrix, new_row]) # Add the new row
 
     def reset_original_matrix(self):
         """