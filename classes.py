4dbb9bc2ac0c6e3cdba39058b2f10c6b67e9316c
anderulf <ulfan@outlook.com>
2020-09-24 15:28:54 +0200

    Merged expansion methods in jacobian class

--- classes.py
+++ classes.py
@@ -463,32 +463,33 @@ class Jacobian:
                     self.matrix[i + self.n, j + self.n] = abs(buses[i + 1].voltage) * (self.y_bus[i, j].real * np.sin(buses[i + 1].delta - buses[j + 1].delta) - self.y_bus[i, j].imag * np.cos(buses[i + 1].delta - buses[j + 1].delta))
             #j_offset = 1
 
-    def predictor_phase_expand(self, alpha_list, beta_list):
+    def continium_expand(self, parameter, alpha_list, beta_list):
         """
         This method is used for Continium Power Flow where the jacobian matrix is expanded with one row and one column
+
+        Parameter should be a string type input with value either "voltage" or "load". "load" input is also used in
+        predictor phase.
 
         Note that these values should be removable aswell
         """
         self.cols = self.m + 1
         self.rows = self.m + 1
-        new_row = [0] * self.m
-        new_col = [0] * (self.m +1)
-        for i in range(len(alpha_list)):
-            new_col[i] = [alpha_list[i]]
+        new_row = [0] * (self.m +1)
+        new_col = [0] * self.m
+        for i in range(len(beta_list)):
+            new_col[i] = [beta_list[i]]
             print("i: ",i)
-        for j in range(1, len(beta_list) +1):
+        for j in range(1, len(alpha_list) +1):
             print("i+j = ", i+j)
-            new_col[i+j] = [beta_list[j-1]] # offset with i
-        # Add last 1 which is the new diagonal element in the jacobian
-        new_col[-1] = [1]
-        self.matrix = np.append(self.matrix, [new_row], 0) # Add zeros on the bottom of the jacobian
+            new_col[i+j] = [alpha_list[j-1]] # offset with i
+        # Add row element which is based on phase
+        if parameter == "load":
+            new_row[-1] = [1] # index -1 is the last element ie. the diagonal element
+        elif parameter == "voltage":
+            new_row[-2] = [1] # index -2 is the second last element ie. the last voltage value (in 3 bus system bus 2)
+        else: print("Error: The parameter in Jacobian.continium_expand is not a valid input.")
         self.matrix = np.append(self.matrix , new_col, 1) # new_col is in format [[],[],[]]
-
-    def corrector_constant_load_expand(self):
-        pass
-
-    def corrector_constant_voltage_expand(self):
-        pass
+        self.matrix = np.append(self.matrix, [new_row], 0)  # Add the new row at the bottom
 
     def reset_original_matrix(self):
         """
