33ca2983d8568643c94f06e95b918c5c85085f26
anderulf <ulfan@outlook.com>
2020-10-02 16:22:50 +0200

    Implemented all tasks and it converges but not idealy

--- continuation/run_continuation.py
+++ continuation/run_continuation.py
@@ -18,7 +18,7 @@ Q = {"1": -0.5, "2": -0.5, "3": None}
 # P values from project
 P = {"1": -1, "2": -0.5, "3": None}
 # Continuation load increase parameters
-beta = {"1": 0.5, "2": 0.5, "3": None}
+beta = {"1": 0.3, "2": 0.7, "3": None}
 alpha = {"1": 0, "2": 0, "3": None}
 
 # line data
@@ -41,38 +41,32 @@ continuation.initialize(max_voltage_step, max_load_step)
 
 # Calculate initial load flow (Point A)
 
-print("1.")
+print("Task 1.")
 while continuation.power_error() > 0.0001:
     continuation.iteration += 1
     continuation.reset_values()
     print("\nIteration: {}\n".format(continuation.iteration))
     continuation.calc_new_power_injections()
     continuation.error_specified_vs_calculated()
-    continuation.print_buses()
     continuation.jacobian.create()
     continuation.find_x_diff()
     continuation.update_values()
-    continuation.print_matrices()
     if continuation.diverging():
         print("No convergence")
         break
 
 # Start predictions and corrections
 #2.
-print("2.")
+print("Task 2.")
 print("\n*--- Predictor phase ---*\n")
 continuation.initialize_predictor_phase()
 continuation.reset_values()
 continuation.find_x_diff()
-continuation.print_matrices()
-
 
 #3.
-print("3.")
-continuation.step = 1;
-continuation.increment_values()
-continuation.update_values() # finished predictor phase
-continuation.print_matrices()
+print("Task 3.")
+continuation.step = 0.3;
+continuation.update_continuation_values()
 
 print("\n*--- Corrector phase ---*\n")
 continuation.iteration = 0
@@ -82,47 +76,51 @@ continuation.error_specified_vs_calculated()
 while continuation.power_error() > 0.0001:
     continuation.iteration += 1
     continuation.reset_values()
-    print("************Bus 1 voltage:", continuation.buses_dict[1].voltage)
     print("\nIteration: {}\n".format(continuation.iteration))
-    print("Current x vector:\n", continuation.x_new)
     continuation.calc_new_power_injections()
     continuation.error_specified_vs_calculated()
     continuation.jacobian.reset_original_matrix()
     continuation.jacobian.create()
     continuation.jacobian.continuation_expand(continuation_parameter, continuation.buses_dict)
     continuation.find_x_diff()
-
-    #continuation.increment_values(continuation_parameter)
-    #continuation.update_values()
-    continuation.print_matrices()
+    continuation.update_values()
     if continuation.diverging():
         print("No convergence")
         break
 continuation.print_matrices()
-
-"""
 #4.
-print("4.")
+print("Task 4.")
+print("\n*--- Predictor phase ---*\n")
+continuation.store_values()
 continuation.initialize_predictor_phase()
 continuation.reset_values()
 continuation.find_x_diff()
-continuation.increment_values()
+continuation.update_continuation_values()
 continuation.print_matrices()
+constant_bus_index = continuation.constant_voltage_bus()
 
 #5.
-print("5.")
-continuation.initialize_corrector_phase("voltage")
+print("Task 5.")
+print("\n*--- Corrector phase ---*\n")
+continuation.iteration = 0
+continuation_parameter = "voltage"
+continuation.initialize_corrector_phase(continuation_parameter, constant_bus_index)
+continuation.error_specified_vs_calculated()
 while continuation.power_error() > 0.0001:
     continuation.iteration += 1
     continuation.reset_values()
     print("\nIteration: {}\n".format(continuation.iteration))
     continuation.calc_new_power_injections()
     continuation.error_specified_vs_calculated()
-    continuation.print_buses()
+    continuation.jacobian.reset_original_matrix()
     continuation.jacobian.create()
-    continuation.update_values()
+    continuation.jacobian.continuation_expand(continuation_parameter, continuation.buses_dict, constant_bus_index)
+    continuation.find_x_diff()
+    print(continuation.x_diff)
+    continuation.S = continuation.x_diff[-1][0]
+    continuation.update_continuation_values()
     continuation.print_matrices()
-    if continuation.diverging():
+    #if continuation.diverging():
+    if continuation.iteration > 100:
         print("No convergence")
         break
-"""
