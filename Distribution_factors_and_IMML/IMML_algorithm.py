3ffe32a4057e59b46471e669f3b8a8bfd428c1aa
anderulf <ulfan@outlook.com>
2020-10-19 12:12:49 +0200

    Fixed use of scalar values in IMML

--- Distribution_factors_and_IMML/IMML_algorithm.py
+++ Distribution_factors_and_IMML/IMML_algorithm.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outage, printing=True):
+def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outage, h_modification=1, printing=True):
     """
     Outage should be a string in the format "x-y" where x is the from bus, and y is the to bus
     The buses are changed by the algorithm so the bus data can be used after the algorithm is run
 
     Printing is set to True by default
+
+    h_modification is a float or integer used to modify a line in the network. Defaulted to 1 if everything is normal
+    Set h_modification to 0.5 if a line is removed
+    or h_modification to 2 if a line is added
     """
     H = np.zeros([len(buses), len(buses)], dtype=float)
     for line in lines:
@@ -22,7 +26,7 @@ def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outa
     from_bus, to_bus = outage.split("-")
     from_bus = int(from_bus)
     to_bus = int(to_bus)
-    delta_h = H[from_bus-1, to_bus-1]/2
+    delta_h = H[from_bus-1, to_bus-1]*h_modification
     M = np.zeros([len(buses)-1, 1])
     M[from_bus-1] = 1
     M[to_bus-1] = -1
@@ -35,12 +39,12 @@ def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outa
         else:
             P_array[int(bus)-1] = specified_active_powers[bus]
     delta_0 = np.linalg.solve(H, P_array)
-    temp_correction_2 = np.matmul(np.transpose(M), delta_0)
-    c_inverse = 1/delta_h + np.linalg.inv(H)[from_bus-1, from_bus-1] - np.linalg.inv(H)[from_bus-1, to_bus-1]
+    temp_correction_2 = np.matmul(np.transpose(M), delta_0)[0][0] # Is a scalar value
+    c_inverse = 1/delta_h + np.linalg.inv(H)[from_bus-1, from_bus-1] - np.linalg.inv(H)[from_bus-1, to_bus-1] # Is generally a scalar value
+    # Utilizing scalar values is a lot more efficiency compared to using matrix multiplication for larger systems
     c = 1/c_inverse
     delta_correction_temp_1 = c * temp_correction_2
-    #delta_correction = -x * delta_correction_temp_1
-    delta_correction = np.matmul(-x, delta_correction_temp_1)
+    delta_correction = -x * delta_correction_temp_1
     delta = delta_0 + delta_correction
     index = 0
     for bus in buses.values():
@@ -60,4 +64,5 @@ def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outa
         print("\nc_inverse: \n", c_inverse)
         print("\nc: \n", c)
         print("\ndelta_corrections: \n", delta_correction)
-
+        print("\nNew angles: \n", delta, "\n")
+    return from_bus, to_bus
