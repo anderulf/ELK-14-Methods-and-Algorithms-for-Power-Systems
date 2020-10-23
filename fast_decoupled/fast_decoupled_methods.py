3feac10c9bfede6343217395d7be77a63baa6edc
trudebyre <trudebyre@gmail.com>
2020-10-23 16:15:14 +0200

    Har endret i oving 3, fast_decoupled_methods

--- fast_decoupled/fast_decoupled_methods.py
+++ fast_decoupled/fast_decoupled_methods.py
@@ -7,12 +7,12 @@ def run_primal_method(fast_dec, printing=False):
     returns the number of iterations
     """
     fast_dec.iteration = 0
+    # Calculate initial active power injections
+    fast_dec.calculate_P_injections()
+    # Calculate the initial mismatches
+    fast_dec.calculate_fast_decoupled_mismatches("P")
     while fast_dec.power_error() > 0.0001:
         fast_dec.iteration += 1
-        # Calculate active power injections
-        fast_dec.calculate_P_injections()
-        # Calculate the mismatches
-        fast_dec.calculate_fast_decoupled_mismatches("P")
         # Calculate the corrections for angles
         theta_correction = np.linalg.solve(fast_dec.B_p, fast_dec.mismatch.get_P())
         # Update theta i X-vector, theta_new = theta_correction + theta_old
@@ -24,6 +24,14 @@ def run_primal_method(fast_dec, printing=False):
         voltage_correction = np.linalg.solve(fast_dec.B_dp, fast_dec.mismatch.get_Q())
         # Update voltage i X-vector, V_new = V_correction + v_old
         fast_dec.update_fast_decoupled_voltage_or_angle(voltages=voltage_correction)
+
+        # Calculate updated active power injections and reactive power injections
+        fast_dec.calculate_P_injections()
+        fast_dec.calculate_Q_injections()
+        # Calculate the mismatches
+        fast_dec.calculate_fast_decoupled_mismatches("P")
+        fast_dec.calculate_fast_decoupled_mismatches("Q")
+
         if printing:
             print_title3("Iteration {}".format(fast_dec.iteration))
             fast_dec.print_data(theta_correction, voltage_correction)
@@ -38,11 +46,11 @@ def run_dual_method(fast_dec, printing=False):
     Returns the number of iterations
     """
     fast_dec.iteration = 0
+    # Calculate initial reactive power injections and mismatches
+    fast_dec.calculate_Q_injections()
+    fast_dec.calculate_fast_decoupled_mismatches("Q")
     while fast_dec.power_error() > 0.0001:
         fast_dec.iteration += 1
-        # Calculate reactive power injections
-        fast_dec.calculate_Q_injections()
-        fast_dec.calculate_fast_decoupled_mismatches("Q")
         # Voltage_correction = B_dp.invers*Q_mismatch(v, theta_updated) (samme som over)
         voltage_correction = np.linalg.solve(fast_dec.B_dp, fast_dec.mismatch.get_Q())
         # Update voltage i X-vector, V_new = V_correction + v_old
@@ -56,6 +64,14 @@ def run_dual_method(fast_dec, printing=False):
         theta_correction = np.linalg.solve(fast_dec.B_p, fast_dec.mismatch.get_P())
         # Update theta in X-vector, theta_new = theta_correction + theta_old
         fast_dec.update_fast_decoupled_voltage_or_angle(angles=theta_correction)
+
+        # Calculate updated active power injections and reactive power injections
+        fast_dec.calculate_P_injections()
+        fast_dec.calculate_Q_injections()
+        # Calculate the mismatches
+        fast_dec.calculate_fast_decoupled_mismatches("P")
+        fast_dec.calculate_fast_decoupled_mismatches("Q")
+
         if printing:
             print_title3("Iteration {}".format(fast_dec.iteration))
             fast_dec.print_data(theta_correction, voltage_correction)
@@ -64,18 +80,14 @@ def run_dual_method(fast_dec, printing=False):
             break
     return fast_dec.iteration
 
-def run_standard_method(fast_dec, printing=False):
-    """
-    Implements the standard fast decoupled method.
-    This method utilizes a very simple jacobian
-    """
+def run_standard_method(fast_dec, printing=False):  # Standard is implemented in the same way as the primal algorithm
     fast_dec.iteration = 0
+    # Calculate initial active power injections
+    fast_dec.calculate_P_injections()
+    # Calculate initial active power mismatches
+    fast_dec.calculate_fast_decoupled_mismatches("P")
     while fast_dec.power_error() > 0.0001:
         fast_dec.iteration += 1
-        # Calculate active power injections
-        fast_dec.calculate_P_injections()
-        # Calculate the mismatches
-        fast_dec.calculate_fast_decoupled_mismatches("P")
         # Calculate the corrections for angles
         theta_correction = np.linalg.solve(fast_dec.B_p, fast_dec.mismatch.get_P())
         # Update theta i X-vector, theta_new = theta_correction + theta_old
@@ -87,6 +99,13 @@ def run_standard_method(fast_dec, printing=False):
         voltage_correction = np.linalg.solve(fast_dec.B_dp, fast_dec.mismatch.get_Q())
         # Update voltage i X-vector, V_new = V_correction + v_old
         fast_dec.update_fast_decoupled_voltage_or_angle(voltages=voltage_correction)
+        # Calculate updated active power injections and reactive power injections
+        fast_dec.calculate_P_injections()
+        fast_dec.calculate_Q_injections()
+        # Calculate the mismatches
+        fast_dec.calculate_fast_decoupled_mismatches("P")
+        fast_dec.calculate_fast_decoupled_mismatches("Q")
+
         if printing:
             print_title3("Iteration {}".format(fast_dec.iteration))
             fast_dec.print_data(theta_correction, voltage_correction)
