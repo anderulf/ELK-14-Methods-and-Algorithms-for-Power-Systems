06ef7ddef71d85539650110d9211c92c2da233a5
trudebyre <trudebyre@gmail.com>
2020-10-23 18:22:22 +0200

    har endret IMML i assignment 4

--- Distribution_factors_and_IMML/IMML_algorithm.py
+++ Distribution_factors_and_IMML/IMML_algorithm.py
@@ -30,8 +30,15 @@ def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outa
     M = np.zeros([len(buses)-1, 1])
     M[from_bus-1] = 1
     M[to_bus-1] = -1
-    # X-vector
+
+    # X-vector = H_invers * M
+    #x = H_invers[:,from_bus-1] - H_invers[:,to_bus-1]
     x = np.linalg.solve(H, M)
+
+    #z = M_transpose * x = M_transpose *H_invers*M
+    z = x[from_bus-1] - x[to_bus -1]
+    print("z = ", z)
+
     P_array = np.zeros([len(buses)-1, 1])
     for bus in specified_active_powers:
         if int(bus) == slack_bus_number:
@@ -39,13 +46,18 @@ def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outa
         else:
             P_array[int(bus)-1] = specified_active_powers[bus]
     delta_0 = np.linalg.solve(H, P_array)
-    temp_correction_2 = np.matmul(np.transpose(M), delta_0)[0][0] # Is a scalar value
-    c_inverse = 1/delta_h + np.linalg.inv(H)[from_bus-1, from_bus-1] - np.linalg.inv(H)[from_bus-1, to_bus-1] # Is generally a scalar value
+
+    #M_transpose*H_invers*P
+    angle_diff = (delta_0[from_bus-1] - delta_0[to_bus-1])[0]
+
+    #temp_correction_2 = np.matmul(np.transpose(M), delta_0)[0][0] # Is a scalar value
+
+    c_inverse = (1/delta_h + z)[0] # Is generally a scalar value
     # Utilizing scalar values is a lot more efficiency compared to using matrix multiplication for larger systems
     # Special Case occurs when running several modifaction in the systems simustaneously
     c = 1/c_inverse
-    delta_correction_temp_1 = c * temp_correction_2
-    delta_correction = -x * delta_correction_temp_1
+    #delta_correction_temp_1 = c * angle_diff
+    delta_correction = -x * c * angle_diff
     delta = delta_0 + delta_correction
     index = 0
     for bus in buses.values():
@@ -59,11 +71,11 @@ def IMML_algorithm(specified_active_powers, buses, lines, slack_bus_number, outa
         print("\ndelta_h: ", delta_h)
         print("\nM:\n", M)
         print("\nP_array: \n", P_array)
-        print("\nDelta_0:\n ", np.round(delta_0, 3))
-        print("\nX-vector: \n", np.round(x, 3))
-        print("\nTemp correction 2: \n", round(temp_correction_2, 3))
-        print("\nc_inverse: \n", round(c_inverse, 3))
-        print("\nc: \n", round(c, 3))
-        print("\ndelta_corrections: \n", np.round(delta_correction, 3))
-        print("\nNew angles: \n", np.round(delta, 3), "\n")
+        print("\nDelta_0:\n ", np.round(delta_0, 4))
+        print("\nX-vector: \n", np.round(x, 4))
+        print("\nTemp correction 2: \n", np.round(angle_diff, 4))
+        print("\nc_inverse: \n", np.round(c_inverse, 4))
+        print("\nc: \n", np.round(c, 4))
+        print("\ndelta_corrections: \n", np.round(delta_correction, 4))
+        print("\nNew angles: \n", np.round(delta, 4), "\n")
     return from_bus, to_bus
