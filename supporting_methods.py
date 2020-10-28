7d1329095959d3a35f4ac9a1f0e3ebcd9ebd5a37
anderulf <ulfan@outlook.com>
2020-10-28 19:44:55 +0100

    Moved dist factors from file

--- supporting_methods.py
+++ supporting_methods.py
@@ -1,10 +1,5 @@
-from typing import Dict, Any
-
 import numpy as np
 from copy import deepcopy
-
-from numpy.core._multiarray_umath import ndarray
-
 
 def rectangular_to_polar(complex_number):
     r = np.sqrt(complex_number.real*complex_number.real+complex_number.imag*complex_number.imag).real
@@ -82,30 +77,3 @@ def create_simplified_y_bus(B, lines, slack_bus_number):
     B = np.delete(B, slack_bus_number - 1, axis=1)
     return B
 
-def calculate_distribution_factors(B_p, P_array, buses, lines, slack_bus_number, avoid_line=None, printing=True):
-    """
-    Runs a distribution factor calculation
-    """
-    right_hand_side_dict = {}
-    a_dict = {}
-    right_hand_side = np.zeros([len(P_array), 1])
-    for line in lines:
-        for bus in buses.values():
-            if bus.bus_number == slack_bus_number:
-                pass
-            elif bus.bus_number == line.from_bus.bus_number:
-                right_hand_side[int(bus.bus_number) - 1] = 1 / line.reactance
-            elif bus.bus_number == line.to_bus.bus_number:
-                right_hand_side[int(bus.bus_number) - 1] = -1 / line.reactance
-            else:
-                right_hand_side[int(bus.bus_number) - 1] = 0
-        right_hand_side_dict[line.name] = deepcopy(right_hand_side)
-        a = np.linalg.solve(B_p, right_hand_side)
-        a_dict[line.name] = a
-        line.p_power_flow = np.matmul(np.transpose(a), P_array)
-        if line.name != avoid_line and printing:
-            print_title3("{}:".format(line.name))
-            print("\nRight hand side: \n", np.round(right_hand_side_dict[line.name], 4))
-            print("\nDistribution factors: \n", np.round(a_dict[line.name], 4))
-            print("\nPower flow on line:\n", "{}pu".format(round(line.p_power_flow[0][0], 4)))
-    return right_hand_side_dict, a_dict
